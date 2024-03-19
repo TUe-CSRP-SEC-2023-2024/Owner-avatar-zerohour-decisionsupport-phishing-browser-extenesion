7b7348185282696138c4b4c52a88c81ef7899b02
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-03-19 15:58:39 +0100

    Load and display current ip settings

--- AntiPhishing/options.js
+++ AntiPhishing/options.js
@@ -12,6 +12,12 @@ chrome.tabs.query(
     let saveButton = document.getElementById("save-button");
     let deleteDataButton = document.getElementById("delete-data-button");
 
+    chrome.storage.local.get(["host"], function (result) {
+      httpsCheckbox.checked = result.host.includes("https");
+      serverIPField.value = result.host.split(":")[1].substring(2);
+      serverPortField.value = result.host.split(":")[2];
+    });
+
     saveButton.addEventListener("click", () => {
       let http = httpsCheckbox.checked ? "https://" : "http://";
       let host = http + serverIPField.value + ":" + serverPortField.value;
