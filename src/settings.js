fb7ae367e7396ee78639109932a08081f30f36c8
TPGamesNL <pet.teun.03@gmail.com>
2024-04-09 16:09:33 +0200

    Add delete cache button

--- src/settings.js
+++ src/settings.js
@@ -1,17 +1,16 @@
-import { clearStorage } from "./storage.js";
+import { clearStorage, clearCache } from "./storage.js";
 
-chrome.tabs.query(
-  {
-    active: true,
-    lastFocusedWindow: true,
-  },
-  (tabs) => {
-    let deleteDataButton = document.getElementById("delete-data-button");
+let deleteDataButton = document.getElementById("delete-data-button");
 
-    deleteDataButton.addEventListener("click", () => {
-      clearStorage();
-      console.log("Data deleted successfully!");
-      chrome.runtime.reload();
-    });
-  }
-);
+deleteDataButton.addEventListener("click", async () => {
+  await clearStorage();
+  alert("Data deleted successfully!");
+  chrome.runtime.reload();
+});
+
+let deleteCacheButton = document.getElementById("delete-cache-button");
+
+deleteCacheButton.addEventListener("click", async () => {
+  await clearCache();
+  alert("Cache deleted successfully!");
+});
